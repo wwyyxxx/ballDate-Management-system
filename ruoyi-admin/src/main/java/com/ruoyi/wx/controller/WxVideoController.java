67389b52ece98302d40d67a955624c3a42dd5640
wyx <YongXiang_Wang@vtech.com>
2020-04-27 17:22:41 +0800

    审核视频

--- ruoyi-admin/src/main/java/com/ruoyi/wx/controller/WxVideoController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/wx/controller/WxVideoController.java
@@ -43,7 +43,7 @@ public class WxVideoController extends BaseController
 //    @RequiresPermissions("wx:video:list")
     @PostMapping("/list")
     @ResponseBody
-    public TableDataInfo list(WxVideo wxVideo)
+    public TableDataInfo list(@RequestBody WxVideo wxVideo)
     {
         startPage();
         List<WxVideo> list = wxVideoService.selectWxVideoList(wxVideo);
